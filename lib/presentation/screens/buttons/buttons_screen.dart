f6e5df99e903be0f4d8097b9158fd3a8ff7a4c9b
Frenyi Guevara <frenyi.guevara@elaniin.com>
2026-01-27 09:36:46 -0400

    Botones agregados

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   const ButtonsScreen({super.key});
@@ -7,7 +8,48 @@ class ButtonsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Buttons Screen')),
-      body: Placeholder(),
+      body: _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.pop();
+        },
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text('Elevated Button')),
+            TextButton(onPressed: () {}, child: Text('Text Button')),
+            OutlinedButton(onPressed: () {}, child: Text('Outlined Button')),
+            FilledButton(onPressed: () {}, child: Text('Filled Button')),
+            IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.remove),
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(theme.primary),
+                iconColor: WidgetStatePropertyAll(Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
